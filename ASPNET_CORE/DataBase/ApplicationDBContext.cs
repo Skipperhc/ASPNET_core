026545085c3d1f4d0fa3d95478bbd37123ddab72
skipperhc <vitorhainosz2@gmail.com>
2020-01-17 16:47:17 -0300

    salvando em banco e outras cparadinhas

--- ASPNET_CORE/DataBase/ApplicationDBContext.cs
+++ ASPNET_CORE/DataBase/ApplicationDBContext.cs
@@ -3,12 +3,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ASPNET_CORE.Models;
 
 namespace ASPNET_CORE.DataBase {
     public class ApplicationDBContext : DbContext {
 
-        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) {
+        public DbSet<Funcionario> Funcionarios { get; set; }
 
+
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) {
         }
+
     }
 }
---
+++ ASPNET_CORE/Models/Funcionario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNET_CORE.Models {
+    public class Funcionario {
+        public int Id{ get; set; }
+        public string Nome { get; set; }
+        public float Salario { get; set; }
+        public string Cpf { get; set; }
+
+        public override string ToString() {
+            return $"{Id} {Nome} {Salario} {Cpf}";
+        }
+    }
+}
