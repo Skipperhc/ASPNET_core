60a167db168960ea45bdba1bccaebf35b367d8d4
skipperhc <vitorhainosz2@gmail.com>
2020-01-17 16:51:57 -0300

    salvando em banco

---
+++ ASPNET_CORE/Controllers/FuncionariosController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ASPNET_CORE.Models;
+using ASPNET_CORE.DataBase;
+using System.Linq;
+using System;
+
+namespace ASPNET_CORE.Controllers {
+    public class FuncionariosController : Controller {
+
+
+        private readonly ApplicationDBContext DataBase;
+
+        public FuncionariosController(ApplicationDBContext database) {
+            this.DataBase = database;
+        }
+
+        public IActionResult Index() {
+            var funcionarios = DataBase.Funcionarios.ToList();
+            return View(funcionarios);
+        }
+
+        public IActionResult Cadastrar() {
+            return View();
+        }
+        public IActionResult Editar() {
+            return View("Cadastrar");
+        }
+        
+        [HttpPost]
+        public IActionResult Salvar(Funcionario funcionario) {
+            DataBase.Funcionarios.Add(funcionario);
+            DataBase.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ ASPNET_CORE/DataBase/ApplicationDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASPNET_CORE.Models;
+
+namespace ASPNET_CORE.DataBase {
+    public class ApplicationDBContext : DbContext {
+
+        public DbSet<Funcionario> Funcionarios { get; set; }
+
+
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) {
+        }
+
+    }
+}
